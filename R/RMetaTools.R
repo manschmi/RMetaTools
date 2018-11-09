4af3c6166991b5c8513030e50799f45add738683
manschmi <ms@mbg.au.dk>
2018-11-09 10:46:41 +0100

    added package basics

---
+++ R/RMetaTools.R
@@ -0,0 +1,13 @@
+#' RMetaTools: Compendium of Tools for Metagene Analysis.
+#'
+#' Uses and depends on python-based deeptools and C-based bwtool.
+#'
+#' @section RMetaTools functions:
+#' to doo
+#'
+#'
+#' @docType package
+#' @name RMetaTools
+#' @import dplyr
+#' @import magrittr
+NULL
