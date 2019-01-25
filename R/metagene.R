aaa58d9614aa1ac9efa0d0e4cbc6fd8c0eabf2fb
manschmi <ms@mbg.au.dk>
2019-01-25 09:56:50 +0100

    fixed recognition of bed file name as input

--- R/metagene.R
+++ R/metagene.R
@@ -3,11 +3,11 @@
 #' Creates regions for metagene analysis.
 #'
 #' @param bed filename for bed file.
-#' @param anchor anchor point TSS, TES or center (default='TSS')
+#' @param anchor anchor point TSS, TES, center or NULL (default='TSS')
 #' @param upstream bp upstream of anchor (default=1000)
 #' @param downstream bp downstream of anchor (default=1000)
 #'
-#' @details Loads the bed file, extracts the position of the anchor point and creates ranges from x bp upstream to x bp downstream.
+#' @details Loads the bed file. If anchor is NULL, returns region from TSS-upstream to TES+downstream. If anchor, extracts the position of the anchor point and creates ranges from x bp upstream to x bp downstream of the anchor point.
 #'
 #' @return GRanges with regions.
 #'
@@ -22,7 +22,20 @@ meta_regions <- function(bed,
                         upstream = 1000,
                         downstream = 1000) {
 
-  anno <- rtracklayer::import(bed)
+  if( class(bed) == 'GRanges' ) {
+     anno <- bed
+  } else if ( class(bed) == 'character' ) {
+    tryCatch(
+      anno <- rtracklayer::import(bed),
+      error = function(c) {
+        c$message <- paste0("Error while trying to create metagene regions from bed file: ", anno, '\n', c$message)
+        stop(c)
+      }
+    )
+  } else {
+    stop(paste0('failed to interpret regions', anno))
+  }
+
   anno$original_start <- start(anno)
   anno$original_end <- end(anno)
   if( !('name' %in% colnames(mcols(anno))) ) {
@@ -30,16 +43,21 @@ meta_regions <- function(bed,
   }
 
   strands <- as.vector(strand(anno))
-  anchor_position <- case_when(
-    (anchor == 'TSS' & strands == '-') ~ end(anno),
-    anchor == 'TSS' ~ start(anno),
-    anchor == 'TES' & strands == '-' ~ start(anno),
-    anchor == 'TES' ~ end(anno),
-    anchor == 'center' ~ as.integer(start(anno) + (end(anno)-start(anno))/2)
-  )
 
-  start(anno) <- ifelse(strand(anno) == '+', anchor_position - upstream, anchor_position - downstream)
-  end(anno) <- ifelse(strand(anno) == '+', anchor_position + downstream - 1, anchor_position + upstream - 1) #0 counts as first position
+  if(is.null(anchor)){
+    start(anno) <- ifelse(strand(anno) == '+', start(anno) - upstream, start(anno) - downstream)
+    end(anno) <- ifelse(strand(anno) == '+', end(anno) + downstream - 1, end(anno) + upstream - 1) #0 counts as first position
+  }else{
+    anchor_position <- case_when(
+      (anchor == 'TSS' & strands == '-') ~ end(anno),
+      anchor == 'TSS' ~ start(anno),
+      anchor == 'TES' & strands == '-' ~ start(anno),
+      anchor == 'TES' ~ end(anno),
+      anchor == 'center' ~ as.integer(start(anno) + (end(anno)-start(anno))/2)
+    )
+    start(anno) <- ifelse(strand(anno) == '+', anchor_position - upstream, anchor_position - downstream)
+    end(anno) <- ifelse(strand(anno) == '+', anchor_position + downstream - 1, anchor_position + upstream - 1) #0 counts as first position
+  }
 
   return(anno)
 }
@@ -54,9 +72,8 @@ meta_regions <- function(bed,
 #' @param anno GRanges object of regions to query.
 #' @param upstream bp upstream of anchor (default=1000)
 #' @param downstream bp downstream of anchor (default=1000)
-#' @param entire_region use entire region (default=FALSE)
 #'
-#' @details Loads the bed file, extracts the position of the anchor point and creates ranges from x bp upstream to x bp downstream.
+#' @details Loads the bed file, extracts the position of the anchor point and creates ranges from x bp upstream to x bp downstream. I upstream and downstream are both NULL, collects signal within region specified in anno.
 #'
 #' @return matrix with columns from most upstream to most downstream and rows are the individual regions from specified strand. rownames are set as name column from anno. colnames are set to relative position of each nt
 #'
@@ -68,14 +85,14 @@ meta_regions <- function(bed,
 #' mat <- get_single_strand_matrix(bw=bw, anno=regions, upstream=1000, downstream=5000)
 #'
 #' @export
-get_single_strand_matrix <- function(bw, anno, upstream=1000, downstream=1000, entire_region=FALSE) {
+get_single_strand_matrix <- function(bw, anno, upstream=1000, downstream=1000) {
 
   ##handling specific strand and order to ensure correct output naming
   seqlevels(anno) <- sort(seqlevels(anno))
   anno <- sort(anno)
 
   ##handling uneven region sizes without defined window size
-  if(entire_region) {
+  if(is.null(upstream) & is.null(downstream)) {
     message('using scale region mode, upstream and downstream arguments are ignored')
     upstream <- 0
     downstream <- max(width(anno))
@@ -200,10 +217,16 @@ collapse_to_n_windows <- function(anno, mat, collapse_fun = mean, n_windows = 10
 #' @param anno GRanges object of regions to query.
 #' @param upstream bp upstream of anchor (default=1000)
 #' @param downstream bp downstream of anchor (default=1000)
-#' @param window_size size for binning in bp (default=1, ie no binning)
-#' @param collapse_fun function for averaging values inside windows (Default = rowMeans, see Details).
+#' @param collapse_to_win_size size for binning in bp (default=1, ie no binning)
+#' @param collapse_to_n_wins number of bins to collapse to (default=NULL, ie no binning)
+#' @param collapse_avg_fun function for averaging values inside windows (Default = rowMeans, see Details).
 #' @param negate_neg_strand_values negate values from minus strand bigwig (default=FALSE).
-#' @details Loads the bed file, extracts the position of the anchor point and creates ranges from x bp upstream to x bp downstream using collapse_fun average over window_size regions. See \code{\link{collapse_to_window_size}} for more info about averaging over window size. negate_neg_strand_values can be set to TRUE to deal with minus strand bigwigs which contain all negative values as used by some labs.
+#' @details Loads the bed file. Anchored mode requires a anchor, upstream and downstream, extracts the position of the anchor point and
+#'   creates ranges from x bp upstream to x bp downstream. I anchor, upstream and downstream are NULL, collects values over specified entire regions.
+#'   If collapse_to_win_size is set to a integer > 0, uses collapse_avg_fun (rowMeans, rowSums or similar) to overage signals within this range See \code{\link{collapse_to_window_size}} for more info about averaging over window size.
+#'   If collapse_to_n_wins is not NULL, ups: this requires collapse_avg_fun to be set to mean, sum or similar (ie instead of the default rowMeans) to overage signals into x equally sized bins. Only used if collapse_to_win_size is not > 1. See \code{\link{collapse_to_n_windows}} for more info about averaging into defined number of windows.
+#'   Negate_neg_strand_values can be set to TRUE to deal with minus strand bigwigs which contain all
+#'   negative values as used by some labs.
 #'
 #' @return matrix with columns from most upstream to most downstream and rows are the individual regions. Rownames are the Name column from the bed file.
 #'
@@ -214,14 +237,23 @@ collapse_to_n_windows <- function(anno, mat, collapse_fun = mean, n_windows = 10
 #' bw_plus <- system.file("extdata", "GSM1573841_mNET_8WG16_siLuc_plus_hg38.bw", package = "RMetaTools")
 #' bw_minus <- system.file("extdata", "GSM1573841_mNET_8WG16_siLuc_minus_hg38.bw", package = "RMetaTools")
 #' mat <- get_matrix(bw_plus, bw_minus, regions, 1000, 5000, 1)
+#' dim(mat)
+#' mat <- get_matrix(bw_plus, bw_minus, regions, 1000, 5000, 50)
+#' dim(mat)
+#' mat <- get_matrix(bw_plus, bw_minus, regions, 1000, 5000, collapse_to_n_wins=5, collapse_avg_fun=mean)
+#' dim(mat)
 #'
 #' #if bigwigs are not stranded, ie from ChIP experiment both plus and minus are the same file
 #' bw <- system.file("extdata", "GSM1573841_mNET_8WG16_siLuc_hg38.bw", package = "RMetaTools")
 #' mat <- get_matrix(bw, bw, regions, 1000, 5000, 50)
 #'
+#'
 #' @export
-get_matrix <- function(bw_plus, bw_minus, anno, upstream=1000, downstream=1000, window_size = 1,
-                       collapse_fun = rowMeans,
+get_matrix <- function(bw_plus, bw_minus, anno,
+                       upstream=1000, downstream=1000,
+                       collapse_to_win_size = NULL,
+                       collapse_to_n_wins = NULL,
+                       collapse_avg_fun = rowMeans,
                        negate_neg_strand_values=FALSE) {
   smat <- get_single_strand_matrix(bw_plus, anno[strand(anno)=="+"], upstream, downstream)
   asmat <- get_single_strand_matrix(bw_minus, anno[strand(anno)=="-"], upstream, downstream)
@@ -229,9 +261,16 @@ get_matrix <- function(bw_plus, bw_minus, anno, upstream=1000, downstream=1000, 
   if (negate_neg_strand_values) {
     asmat <- -asmat
   }
+  if(ncol(smat) > ncol(asmat)){
+    asmat <- cbind(asmat, matrix(NA, nrow=nrow(asmat), ncol=(ncol(smat)-ncol(asmat)) ))
+  }else if(ncol(smat) < ncol(asmat)){
+    smat <- cbind(smat, matrix(NA, nrow=nrow(smat), ncol=(ncol(asmat)-ncol(smat)) ))
+  }
   mat <- rbind(smat, asmat)
-  if(window_size > 1) {
-    mat <- collapse_to_window_size(mat, collapse_fun, window_size)
+  if(!is.null(collapse_to_win_size)) {
+    mat <- collapse_to_window_size(mat, collapse_avg_fun, collapse_to_win_size)
+  }else if(!is.null(collapse_to_n_wins)){
+    mat <- collapse_to_n_windows(anno, mat, collapse_avg_fun, collapse_to_n_wins)
   }
   mat
 }
@@ -279,15 +318,17 @@ mat_to_tbl <- function(mat) {
 #' @param bw_plus bigwig filename for plus strand signal
 #' @param bw_minus bigwig filename for minus strand signal
 #' @param anno Bed filename for annotations to use OR a GRanges object (see Details).
-#' @param anchor 'TSS', 'TES' or 'center.'
+#' @param anchor 'TSS', 'TES', 'center' or NULL (default='TSS')
 #' @param upstream bp upstream of anchor (default=1000)
 #' @param downstream bp downstream of anchor (default=1000)
-#' @param window_size size for binning in bp (default=1, ie no binning)
-#' @param collapse_fun function for averaging values inside windows (Default = rowMeans, see Details).
+#' @param collapse_to_win_size size for binning in bp (default=1, ie no binning)
+#' @param collapse_to_n_wins number of bins to collapse to (default=NULL, ie no binning)
+#' @param collapse_avg_fun function for averaging values inside windows (Default = rowMeans, see Details).
 #' @param negate_neg_strand_values negate values from minus strand bigwig (default=FALSE).
 #'
-#' @details Loads the bed file, extracts the position of the anchor point and creates ranges from x bp upstream to x bp downstream. Queries from both bigwigs and the correct strand. Summarizes signal of window_size bins and returns a tidy data.frame. negate_neg_strand_values can be set to TRUE to deal with minus strand bigwigs which contain all negative values as used by some labs.
+#' @details Loads the bed file. If anchor is given, extracts the position of the anchor point and creates ranges from x bp upstream to x bp downstream. If anchor is NULL, collects signal in specified region from TSS-upstream to TES+downstream. Queries from both bigwigs and the correct strand. Summarizes signal of window_size bins and returns a tidy data.frame. negate_neg_strand_values can be set to TRUE to deal with minus strand bigwigs which contain all negative values as used by some labs.
 #' When using a GRanges object for anno, this assumes the bigwigs will be queries from start to end! All ranges must be exactly same size!
+#'
 #' @return Tidy data frame with columns: gene (name, ie col4 from *bed* file), rel_pos (position in bp relative to *anchor*), value (averaged value from bigwig file).
 #'
 #' @examples
@@ -298,28 +339,49 @@ mat_to_tbl <- function(mat) {
 #' anno=bedfile;anchor='center';upstream=1000;downstream=1000;window_size=10
 #' collapse_fun = rowMeans;negate_neg_strand_values=FALSE
 #' metamat <- metagene_matrix(bw_plus, bw_minus, bedfile, anchor, upstream, downstream, window_size)
+#' metamat <- metagene_matrix(bw_plus, bw_minus, bedfile, anchor, upstream, downstream, collapse_to_n_wins=5, collapse_avg_fun=mean)
+#' bedfile <- system.file("extdata", "snRNA_MS31_hg38.bed", package = "RMetaTools")
+#' metamat <- metagene_matrix(bw_plus, bw_minus, bedfile, anchor, upstream, downstream, window_size)
+#' metamat <- metagene_matrix(bw_plus, bw_minus, bedfile, anchor=NULL, 10, 10, collapse_to_n_wins=5, collapse_avg_fun=mean)
+#'
+#' # for pre-loaded regions (useful for ie modifying regions by hand)
+#' # this most useful
+#' regions <- meta_regions(bedfile, 'TSS', 1000, 5000)
+#' start(regions) <- start(regions) + 1000
+#' end(regions) <- end(regions) - 1000
+#' regions <- regions[width(regions)>0,]
+#' metamat <- metagene_matrix(bw_plus, bw_minus, regions, anchor=NULL, upstream=0, downstream=0, collapse_to_n_wins=5, collapse_avg_fun=mean)
+#' metamat <- metagene_matrix(bw_plus, bw_minus, regions, anchor='TSS', upstream=100, downstream=100, collapse_to_win_size=50, collapse_avg_fun=rowMeans)
+#' metamat <- metagene_matrix(bw_plus, bw_minus, bedfile, anchor='TSS', upstream=100, downstream=100, collapse_to_win_size=50, collapse_avg_fun=rowMeans)
 #'
 #' @export
-metagene_matrix <- function(bw_plus, bw_minus, anno, anchor, upstream=1000, downstream=1000, window_size = 1,
-                            collapse_fun = rowMeans,
+metagene_matrix <- function(bw_plus, bw_minus, anno,
+                            anchor='TSS', upstream=1000, downstream=1000,
+                            collapse_to_win_size = NULL,
+                            collapse_to_n_wins = NULL,
+                            collapse_avg_fun = rowMeans,
                             negate_neg_strand_values=FALSE) {
-  if( class(anno) == 'GRanges' ) {
-    regions <- anno
-  } else if ( is.character(class(anno)) ) {
-    tryCatch(
+
+  meta_args <- c(anchor, upstream, downstream, collapse_to_win_size, collapse_to_n_wins)
+  names(meta_args) <- c('anchor', 'upstream', 'downstream', 'collapse_to_win_size', 'collapse_to_n_wins')
+
+  tryCatch(
       regions <- meta_regions(anno, anchor, upstream, downstream),
       error = function(c) {
-        c$message <- paste0("Error while trying to create metagene regions from bed file: ", anno, '\n', c$message)
+        c$message <- paste0("Error while trying to create metagene regions \n", c$message)
         stop(c)
-      }
-    )
-  } else {
-    stop(paste0('failed to interpret regions', anno))
+      })
+
+  if(is.null(anchor)){
+    upstream <- NULL
+    downstream <- NULL
   }
 
   tryCatch(
-    mat <- get_matrix(bw_plus, bw_minus, regions, upstream, downstream, window_size,
-                                  collapse_fun, negate_neg_strand_values),
+    mat <- get_matrix(bw_plus, bw_minus, regions,
+                      upstream, downstream,
+                      collapse_to_win_size, collapse_to_n_wins,
+                      collapse_avg_fun, negate_neg_strand_values),
     error = function(c) {
       c$message <- paste0("Error while trying to create  metagene matrix.\n", c$message)
       stop(c)
@@ -335,6 +397,10 @@ metagene_matrix <- function(bw_plus, bw_minus, anno, anchor, upstream=1000, down
       stop(c)
     }
   )
+
+  for(i in seq_along(meta_args)){
+    attr(mat, names(meta_args)[i]) <- meta_args[i]
+  }
 
   mat
 }
@@ -356,16 +422,16 @@ metagene_matrix <- function(bw_plus, bw_minus, anno, anchor, upstream=1000, down
 #' @examples
 #'
 #' bedfile <- system.file("extdata", "Chen_PROMPT_TSSs_liftedTohg38.bed", package = "RMetaTools")
-#' regions <- meta_regions(bedfile, 'TSS', 1000, 5000)
 #' bw_plus <- system.file("extdata", "GSM1573841_mNET_8WG16_siLuc_plus_hg38.bw", package = "RMetaTools")
 #' bw_minus <- system.file("extdata", "GSM1573841_mNET_8WG16_siLuc_minus_hg38.bw", package = "RMetaTools")
-#' mat <- get_matrix(bw_plus, bw_minus, regions, 1000, 1000, 50)
-#' tidy_meta <- mat_to_tbl(mat)
-#' plot_htmp(tidy_meta, color_by='value', do_interpolate=FALSE)
+#' anno=bedfile;anchor='center';upstream=1000;downstream=1000;window_size=10
+#' collapse_fun = rowMeans;negate_neg_strand_values=FALSE
+#' metamat <- metagene_matrix(bw_plus, bw_minus, bedfile, anchor, upstream, downstream, window_size)
+#' plot_htmp(metamat, color_by='value', do_interpolate=FALSE)
 #'
 #' #log2 values incl pseudocount
-#' tidy_meta %<>% mutate(log2_value = log2(value+1))
-#' plot_htmp(tidy_meta, color_by='log2_value', do_interpolate=FALSE)
+#' metamat %<>% mutate(log2_value = log2(value+1))
+#' plot_htmp(metamat, color_by='log2_value', do_interpolate=FALSE)
 #'
 #' @export
 plot_htmp <- function(tbl, color_by='value', do_interpolate=FALSE){
@@ -395,12 +461,15 @@ plot_htmp <- function(tbl, color_by='value', do_interpolate=FALSE){
 #' @examples
 #'
 #' bedfile <- system.file("extdata", "Chen_PROMPT_TSSs_liftedTohg38.bed", package = "RMetaTools")
-#' regions <- meta_regions(bedfile, 'TSS', 1000, 5000)
 #' bw_plus <- system.file("extdata", "GSM1573841_mNET_8WG16_siLuc_plus_hg38.bw", package = "RMetaTools")
 #' bw_minus <- system.file("extdata", "GSM1573841_mNET_8WG16_siLuc_minus_hg38.bw", package = "RMetaTools")
-#' mat <- get_matrix(bw_plus, bw_minus, regions, 1000, 1000, 50)
-#' tidy_meta <- mat_to_tbl(mat)
-#' tidy_avgs <- meta_average(tidy_meta)
+#' anno=bedfile;anchor='center';upstream=1000;downstream=1000;window_size=10
+#' collapse_fun = rowMeans;negate_neg_strand_values=FALSE
+#' metamat <- metagene_matrix(bw_plus, bw_minus, bedfile, anchor, upstream, downstream, window_size)
+#' tidy_avgs <- meta_average(metamat)
+#'
+#' #alt:
+#' tidy_log2_avgs <- meta_average(mutate(metamat, value = log2(value+1)))
 #'
 #' @export
 meta_average <- function(tbl){
@@ -425,13 +494,13 @@ meta_average <- function(tbl){
 #' @examples
 #'
 #' bedfile <- system.file("extdata", "Chen_PROMPT_TSSs_liftedTohg38.bed", package = "RMetaTools")
-#' regions <- meta_regions(bedfile, 'TSS', 1000, 5000)
 #' bw_plus <- system.file("extdata", "GSM1573841_mNET_8WG16_siLuc_plus_hg38.bw", package = "RMetaTools")
 #' bw_minus <- system.file("extdata", "GSM1573841_mNET_8WG16_siLuc_minus_hg38.bw", package = "RMetaTools")
-#' mat <- get_matrix(bw_plus, bw_minus, regions, 1000, 1000, 50)
-#' tidy_meta <- mat_to_tbl(mat)
-#' tidy_avgs <- meta_average(tidy_meta)
-#' plot_profile(tidy_avgs)
+#' anno=bedfile;anchor='center';upstream=1000;downstream=1000;window_size=10
+#' collapse_fun = rowMeans;negate_neg_strand_values=FALSE
+#' metamat <- metagene_matrix(bw_plus, bw_minus, bedfile, anchor, upstream, downstream, window_size)
+#' tidy_log2_avgs <- meta_average(mutate(metamat, value = log2(value+1)))
+#' plot_profile(tidy_log2_avgs)
 #'
 #' @export
 plot_profile <- function(meta_tbl, color_by=NULL){
