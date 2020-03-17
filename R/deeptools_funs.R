db3f4821302c0fb9103cc5f2fcc868574262367b
manschmi <ms@mbg.au.dk>
2020-03-17 17:39:47 +0100

    add option to load deeptools to S3 object and metagene aggregation for those objects

--- R/deeptools_funs.R
+++ R/deeptools_funs.R
@@ -93,8 +93,16 @@ load_deeptoolsmatrix <- function(fname, na.omit=TRUE, na.fill=0, as.matrix=FALSE
 #' @param na.omit Skip na values in the matrix. Default is TRUE.
 #' @param na.fill Fills NAs with this value zero. Default is 0.
 #' @param as.matrix Return matrix as matrix (default is FALSE, ie returns tidy data.frame instead)
+#' @param as.S3 Return S3 class metagene object, see details for more info
 #'
-#' @details Loads a gzipped deeptools matrix file and collects this in a \code{tidy} data.frame. If as.matrix is true, the header information is added as attribute.
+#' @details Loads a gzipped deeptools matrix file and collects this in a
+#'   \code{tidy} data.frame. If as.matrix is true, the header information is
+#'   added as attribute.
+#'   as.S3 returns a metagene object, with slots: $header, $regions, $samples,
+#'   $groups and $matrices. Where $header contains the header information as a
+#'   named vector. Matrices is a list of matrices where each matrix is specific
+#'   to 1 sample and 1 group. $samples are the samplenames for each matrix.
+#'   $groups are the groups for each matrix.
 #'
 #' @return An object of class \code{deeptools_matrix} with provided means and covariances
 #'   computed as 1/\code{alpha}.
@@ -108,9 +116,11 @@ load_deeptoolsmatrix <- function(fname, na.omit=TRUE, na.fill=0, as.matrix=FALSE
 #'  head(df_withNA)
 #'  df_withNAas0 <- load_deeptoolsmatrix3(fname, na.omit=FALSE, na.fill=0)
 #'  head(df_withNAas0)
+#'  s3obj <- load_deeptoolsmatrix3(fname, na.omit=FALSE, na.fill=0, as.S3=TRUE)
+#'  head(s3obj$regions)
 #'
 #' @export
-load_deeptoolsmatrix3 <- function(fname, na.omit=TRUE, na.fill=0, as.matrix=FALSE){
+load_deeptoolsmatrix3 <- function(fname, na.omit=TRUE, na.fill=0, as.matrix=FALSE, as.S3=FALSE){
   header <- parse_matrix_header(fname)
   values <- read.table(gzfile(fname), header=F, skip=1)
 
@@ -121,7 +131,9 @@ load_deeptoolsmatrix3 <- function(fname, na.omit=TRUE, na.fill=0, as.matrix=FALS
   body <- as.numeric(header[['body']])
   up <- as.numeric(header[['upstream']])
   dn <- as.numeric(header[['downstream']])
-  xlabels <- lapply(seq_along(bin), function(i) seq(-up[i],body[i]+dn[i]-1,bin[i]))
+  unscaled5 <- as.numeric(header[['unscaled 5 prime']])
+  unscaled3 <- as.numeric(header[['unscaled 3 prime']])
+  xlabels <- lapply(seq_along(bin), function(i) seq(-up[i],unscaled5[i]+body[i]+unscaled3[i]+dn[i]-1,bin[i]))
 
   mgroups <- unlist(header[['group_labels']])
   group_bounds <- as.numeric(unlist(header[['group_boundaries']]))
@@ -139,6 +151,30 @@ load_deeptoolsmatrix3 <- function(fname, na.omit=TRUE, na.fill=0, as.matrix=FALS
     }
     colnames(values) <- c(bednames, name_vec)
     return(values)
+  }
+
+  if (as.S3) {
+    s3 <- structure(list(), class = "metagene")
+    s3$header <- header
+    colnames(bed_info) <- c('chr', 'start', 'end', 'id', 'name', 'strand')
+    s3$regions <- tbl_df(bed_info)
+
+    indeces <- expand.grid(seq_along(msamples), seq_along(mgroups))
+
+    mat_ranges <- data.frame(t(data.frame(sample_start_idx = sample_bounds[indeces[,1]] + 7,
+                                          sample_end_idx = sample_bounds[indeces[,1]+1] + 6,
+                                          group_start_idx = group_bounds[indeces[,2]]+1,
+                                          group_end_idx = group_bounds[indeces[,2]+1])))
+
+    s3$matrices <- lapply(mat_ranges, function(x) {
+      values[x[3]:x[4], x[1]:x[2]]
+    })
+
+    s3$samples <- rep(msamples, length(mgroups))
+    s3$groups <- rep(mgroups, each=length(msamples))
+    s3$rel_pos <- seq(-up,unscaled5+body+unscaled3+dn-1,bin)
+
+    return(s3)
   }
 
   dfs <- list()
@@ -170,6 +206,7 @@ load_deeptoolsmatrix3 <- function(fname, na.omit=TRUE, na.fill=0, as.matrix=FALS
 
   as_data_frame(d)
 }
+
 
 
 
@@ -211,6 +248,67 @@ parse_matrix_header <- function(fname) {
 
 
 
+
+#' metagene
+#'
+#' Metagene aggregate from an S3 metagene object.
+#'
+#' @param obj the object to create the metagene aggregate for
+#' @param aggregate_fun the function name to aggregate values such as 'events', 'sum', 'mean', 'ttest' (default is 'mean')
+#' @param transform_fun a function to be applied to vlues before aggregate function is applied.
+#' @param ... other arguments passed to fun
+#'
+#' @details Aggregates values.
+#' transform_fun takes a matrix as input and returns a similar sized matrix. ie log2p1 <- function(m){log2(m+1)}
+#' aggreagate_fun can be mean, sum, events or t.test or vector containing several of those.
+#'
+#' @return
+#' a tbl_df with column, rel_pos, sample, group, and aggregate values where column name is mean, sum, events or estimate+conf.low+conf.high for ttest
+#' @examples
+#'  fname <- system.file("extdata", "matrix_grouped.gz", package = "RMetaTools")
+#'  df <- load_deeptoolsmatrix3(fname, as.S3=TRUE)
+#'  meta_df <- metagene_aggregate(df)
+#'  meta_df <- metagene_aggregate(df, aggregate_fun = 'events')
+#'  ggplot(meta_df, aes(x=rel_pos, y=events, color=sample)) + geom_line() + facet_grid(group~.)
+#'  meta_df <- metagene_aggregate(df, aggregate_fun = 'ttest')
+#'  log2p1 <- function(m){log2(m+1)}
+#'  meta_df <- metagene_aggregate(df, aggregate_fun = 'ttest', transform_fun = log2p1)
+#'
+#' @export
+metagene_aggregate <- function( obj, aggregate_fun = 'mean', transform_fun = NULL, ... ) {
+
+  if (missing(transform_fun)) {
+    mat_list <- obj$matrices
+  }else{
+    mat_list <- lapply(obj$matrices, transform_fun)
+  }
+
+  meta_df = tbl_df(data.frame(
+    rel_pos = rep(s3$rel_pos, length(s3$matrices)),
+    sample = rep(s3$samples, each=length(s3$rel_pos)),
+    group = rep(s3$groups, each=length(s3$rel_pos))))
+
+  if( 'mean' %in% aggregate_fun ){
+    meta_df$mean = lapply(mat_list, colMeans) %>% simplify
+  }else if( 'sum' %in% aggregate_fun ){
+    meta_df$sum = lapply(mat_list, colSums) %>% simplify
+  }else if( 'events' %in% aggregate_fun ){
+    meta_df$events = lapply(mat_list, function(m) apply(m,2,function(mi) sum(mi > 0))) %>% simplify
+  }else if( 'ttest' %in% aggregate_fun ){
+    meta_ttests <- lapply(mat_list, function(mi) apply(mi,2,t.test))
+
+    meta_df$estimate = lapply(meta_ttests, function(tt) lapply(tt, function(tti) tti$estimate) %>% simplify) %>% simplify
+    meta_df$conf.low =  lapply(meta_ttests, function(tt) lapply(tt, function(tti) tti$conf.int[1]) %>% simplify) %>% simplify
+    meta_df$conf.high =  lapply(meta_ttests, function(tt) lapply(tt, function(tti) tti$conf.int[2]) %>% simplify) %>% simplify
+
+  }
+
+  meta_df
+}
+
+
+
+
 #' plot_heatmap
 #'
 #' Plots tidy dataframe from a deeptools matrix file to a ggplot2 object.
@@ -219,7 +317,7 @@ parse_matrix_header <- function(fname) {
 #' @param sort_y how to sort y axis of the heatmap ('region_len', 'sum', 'mean', 'max' )
 #' @param facets a string for facetting of the plot.
 #'
-#' @details Plots a df from a deeptools matrix file.
+#' @details Plots a df from a deeptools matrix tbl_df.
 #'
 #' @return An ggplot2 plot object.
 #'
@@ -302,3 +400,80 @@ sort_matrix <- function( matrix_file, bed_file ) {
 
   semi_join(m, b)
 }
+
+
+
+
+# ```{r}
+# mod <- function(mats, fun){
+#   lapply(mats, fun)
+# }
+# library(broom)
+# tmat <- list(matrix(1:10,ncol=2), matrix(11:20,ncol=2))
+# names(tmat) <- c('s1', 's2')
+# add1_and_log2 <- function(mat, pseudocount=1){ log2(mat+pseudocount) }
+# ttcols <- function(mat){bind_rows(apply(mat,2,function(col) tidy(t.test(col))))}
+# tmat2 <- mod(tmat, add1_and_log2)
+# tmat_tt <- mod(tmat2, ttcols)
+#
+# tmat3 <- imap(tmat, ~ log2(.x+1))
+# names(tmat3)
+#
+# tmat3 <- imap(tmat, ~ add1_and_log2(., pseudocount=1))
+#
+# transform_fun <- add1_and_log2
+# tmat3 <- imap(tmat, ~ transform_fun(., pseudocount=1))
+#
+# ```
+
+#'  fname <- system.file("extdata", "deeptools3_matrix.gz", package = "RMetaTools")
+#'  df <- load_deeptoolsmatrix3(fname)
+#'  head(df)
+#'  df_withNA <- load_deeptoolsmatrix3(fname, na.omit=FALSE)
+#'  head(df_withNA)
+#'  df_withNAas0 <- load_deeptoolsmatrix3(fname, na.omit=FALSE, na.fill=0)
+#'  head(df_withNAas0)
+#'  s3obj <- load_deeptoolsmatrix3(fname, na.omit=FALSE, na.fill=0, as.S3=TRUE)
+#'  head(df_withNAas0)
+# fname <- '/Users/au220280/ms_tools/MS_Metagene_Tools/test/matrix_grouped.gz'
+#
+# header <- parse_matrix_header(fname)
+# values <- read.table(gzfile(fname), header=F, skip=1)
+#
+# bed_info <- values[,1:6]
+# msamples <- unlist(header[['sample_labels']])
+# sample_bounds <- as.numeric(unlist(header[['sample_boundaries']]))
+# bin <- as.numeric(header[['bin size']])
+# body <- as.numeric(header[['body']])
+# up <- as.numeric(header[['upstream']])
+# unscaled5 <- as.numeric(header[['unscaled 5 prime']])
+# unscaled3 <- as.numeric(header[['unscaled 3 prime']])
+# dn <- as.numeric(header[['downstream']])
+# xlabels <- lapply(seq_along(bin), function(i) seq(-up[i],body[i]+dn[i]-1,bin[i]))
+#
+# mgroups <- unlist(header[['group_labels']])
+# group_bounds <- as.numeric(unlist(header[['group_boundaries']]))
+#
+#
+# indeces <- expand.grid(seq_along(msamples), seq_along(mgroups))
+#
+# mat_ranges <- data.frame(t(data.frame(sample_start_idx = sample_bounds[indeces[,1]] + 7,
+#                      sample_end_idx = sample_bounds[indeces[,1]+1] + 6,
+#                      group_start_idx = group_bounds[indeces[,2]]+1,
+#                      group_end_idx = group_bounds[indeces[,2]+1])))
+#
+# s3$matrices <- lapply(mat_ranges, function(x) {
+#   values[x[3]:x[4], x[1]:x[2]]
+# })
+#
+# s3$samples <- rep(msamples, length(mgroups))
+# s3$groups <- rep(mgroups, each=length(msamples))
+# s3$rel_pos <- seq(-up,unscaled5+body+unscaled3+dn-1,bin)
+#
+# aggfun <- colSums
+#
+#
+#
+# ggplot(meta_ev, aes(x=rel_pos, y=events, color=sample)) +
+#   geom_line() +
+#   facet_wrap(~group)
